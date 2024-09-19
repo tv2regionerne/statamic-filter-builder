8267d7b4a3ac7c0a6f377164b83d4c809684e587
Ryan Mitchell <ryan@thoughtcollective.com>
2024-09-19 07:50:45 +0100

    Use hooks to set query scope automatically

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,9 +3,25 @@
 namespace Tests;
 
 use Statamic\Testing\AddonTestCase;
+use Statamic\Testing\Concerns\PreventsSavingStacheItemsToDisk;
 use Tv2regionerne\StatamicFilterBuilder\ServiceProvider;
 
 class TestCase extends AddonTestCase
 {
+    use PreventsSavingStacheItemsToDisk;
+
+    protected $fakeStacheDirectory = __DIR__.'/__fixtures__/dev-null';
+
     protected string $addonServiceProvider = ServiceProvider::class;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withoutVite();
+
+        if (! file_exists($this->fakeStacheDirectory)) {
+            mkdir($this->fakeStacheDirectory, 0777, true);
+        }
+    }
 }
